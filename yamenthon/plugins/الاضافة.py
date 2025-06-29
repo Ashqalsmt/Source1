1bfca69fb4540f9030650b2974cdd110fbab0458
Ashqalsmt <128143959+Ashqalsmt@users.noreply.github.com>
2025-06-29 13:48:29 +0300

    Update الاضافة.py

--- yamenthon/plugins/الاضافة.py
+++ yamenthon/plugins/الاضافة.py
@@ -72,6 +72,8 @@ async def get_users(event):
                 return await zedb.edit(f"**حالة الإضافة انتهت مع الأخطاء**\n- (**ربما هنالك ضغط على الأمر حاول مجددا لاحقا **) \n**الخطأ** : \n`{error}`\n\n• إضافة `{s}` \n• خطأ بإضافة `{f}`")
             await zedub(InviteToChannelRequest(channel=event.chat_id, users=[user.id]))
             s = s + 1
+            save_user(user.id)  # حفظ المستخدم بعد الإضافة
+            await asyncio.sleep(random.randint(7, 15))  # تأخير عشوائي بين 10 - 30 ثانية
             await zedb.edit(f"**    ⃟⁞⃟⟢ ╎تتم الإضافة **\n\n• إضيف `{s}` \n•  خطأ بإضافة `{f}` \n\n**× اخر خطأ:** `{error}`")
         except Exception as e:
             error = str(e)
